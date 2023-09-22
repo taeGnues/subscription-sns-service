eebf553bf6e0c03f5d4016568ca9e1ab6eed5f74
정태승 <hi563@g.skku.edu>
2023-09-22 20:03:48 +0900

    [종합] EC2 배포용 정보 수정 (generate-ddl, url)

--- src/main/java/com/example/demo/common/oauth/OAuthService.java
+++ src/main/java/com/example/demo/common/oauth/OAuthService.java
@@ -74,35 +74,6 @@ public class OAuthService {
                     return getSocialOAuthRes;
                 }
             }
-//            case GOOGLE: {
-//                //구글로 일회성 코드를 보내 액세스 토큰이 담긴 응답객체를 받아옴
-//                ResponseEntity<String> accessTokenResponse = googleOauth.requestAccessToken(code);
-//                //응답 객체가 JSON형식으로 되어 있으므로, 이를 deserialization해서 자바 객체에 담을 것이다.
-//                GoogleOAuthToken oAuthToken = googleOauth.getAccessToken(accessTokenResponse);
-//
-//                //액세스 토큰을 다시 구글로 보내 구글에 저장된 사용자 정보가 담긴 응답 객체를 받아온다.
-//                ResponseEntity<String> userInfoResponse = googleOauth.requestUserInfo(oAuthToken);
-//                //다시 JSON 형식의 응답 객체를 자바 객체로 역직렬화한다.
-//                GoogleUser googleUser = googleOauth.getUserInfo(userInfoResponse);
-//
-//                //우리 서버의 db와 대조하여 해당 user가 존재하는 지 확인한다.
-//                if(userService.checkUserByEmail(googleUser.getEmail())) { // user가 DB에 있다면, 로그인 진행
-//                    // 유저 정보 조회
-//                    GetUserRes getUserRes = userService.getUserByEmail(googleUser.getEmail());
-//
-//                    //서버에 user가 존재하면 앞으로 회원 인가 처리를 위한 jwtToken을 발급한다.
-//                    String jwtToken = jwtService.createJwt(getUserRes.getId());
-//
-//                    //액세스 토큰과 jwtToken, 이외 정보들이 담긴 자바 객체를 다시 전송한다.
-//                    GetSocialOAuthRes getSocialOAuthRes = new GetSocialOAuthRes(jwtToken, getUserRes.getId(), oAuthToken.getAccess_token(), oAuthToken.getToken_type());
-//                    return getSocialOAuthRes;
-//                }else { // user가 DB에 없다면, 회원가입 진행
-//                    // 유저 정보 저장
-//                    PostUserRes postUserRes = userService.createOAuthUser(googleUser.toEntity());
-//                    GetSocialOAuthRes getSocialOAuthRes = new GetSocialOAuthRes(postUserRes.getJwt(), postUserRes.getId(), oAuthToken.getAccess_token(), oAuthToken.getToken_type());
-//                    return getSocialOAuthRes;
-//                }
-//            }
             default: {
                 throw new BaseException(INVALID_OAUTH_TYPE);
             }
